a69492693b25b86b44227a77f3d430cc404d3ebe
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2022-11-08 19:19:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import ReactDOM from "react-dom";
+
+// Create a Context
+const NumberContext = React.createContext();
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NumberContext.Provider value={24}>
+      <div>
+        <Display />
+      </div>
+    </NumberContext.Provider>
   );
 }
 
-export default App;
+function Display() {
+  
+  return (
+    <NumberContext.Consumer>
+      {value => <div>The answer is {value}.</div>}
+    </NumberContext.Consumer>
+  );
+}
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
+export default App
